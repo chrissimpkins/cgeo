2e5a1d8f26ab977ecd7be8714da21562ee14fd16
Chris Simpkins <git.simpkins@gmail.com>
2021-11-07 10:26:55 -0500

    [lib] update docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,17 @@
 //! A 2D computational geometry library
+//!
+//! ## About
+//!
+//! This crate is a 2D computational geometry library that supports geometric primitives and
+//! fundamental 2D geometric types with computationally efficient algorithms.
+//!
+//! ## Source
+//!
+//! The source files are available at <https://github.com/chrissimpkins/cgeo>.
+//!
+//! ## License
+//!
+//! The project is released under the [Apache License v2.0](https://github.com/chrissimpkins/cgeo/blob/main/LICENSE.md).
 
 #![deny(rustdoc::broken_intra_doc_links, unsafe_code)]
 
